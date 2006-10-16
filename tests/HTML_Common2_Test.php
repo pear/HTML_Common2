cfab93af3b9f32645af26e795d80f6384ea8e2ec
Alexey Borzov <avb@php.net>
2006-10-16 13:06:46 +0000

    Prepare release 0.3.0 (full list of changes is in package2.xml)
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Common2/trunk@221841 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/HTML_Common2_Test.php
+++ tests/HTML_Common2_Test.php
@@ -2,26 +2,82 @@
 /**
  * Unit test suite for HTML_Common2 class
  *
- * @author  Alexey Borzov <avb@php.net>
- * @version $Id$
+ * PHP version 5
+ *
+ * LICENSE:
+ * 
+ * Copyright (c) 2004-2006, Alexey Borzov <avb@php.net>
+ *  
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the 
+ *      documentation and/or other materials provided with the distribution.
+ *    * The names of the authors may not be used to endorse or promote products 
+ *      derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category   HTML
+ * @package    HTML_Common2
+ * @author     Alexey Borzov <avb@php.net>
+ * @license    http://opensource.org/licenses/bsd-license.php New BSD License
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/HTML_Common2
  */
 
 if (!defined('PHPUnit2_MAIN_METHOD')) {
     define('PHPUnit2_MAIN_METHOD', 'this is getting ridiculous');
 }
 
+/**
+ * PHPUnit2 Test Case  
+ */
 require_once 'PHPUnit2/Framework/TestCase.php';
+
+/**
+ * HTML_Common2 class
+ */
 require_once 'HTML/Common2.php';
 
-// HTML_Common2 cannot be instantiated directly
+/**
+ * A non-abstract subclass of HTML_Common2 
+ *
+ * HTML_Common2 cannot be instantiated due to abstract __toString() method,
+ * we need to (sort of) implement that.
+ */
 class HTML_Common2_Concrete extends HTML_Common2
 {
-    public function toHtml()
+    public function __toString()
     {
         return '';
     }
 }
 
+/**
+ * A subclass to test the 'watched attributes' functionality of HTML_Common2 
+ *
+ * Two attributes are watched here: 'readonly' and 'uppercase'. The former
+ * should not be changed by any of the methods and the value of the latter
+ * should always be uppercase. This is achieved by implementing the
+ * onAttributeChange() method defined in HTML_Common2  
+ */
 class HTML_Common2_WatchedAttributes extends HTML_Common2_Concrete
 {
     protected $watchedAttributes = array('readonly', 'uppercase');
@@ -46,6 +102,9 @@ class HTML_Common2_WatchedAttributes extends HTML_Common2_Concrete
     }
 }
 
+/**
+ * Unit test for HTML_Common2 class
+ */
 class HTML_Common2_Test extends PHPUnit2_Framework_TestCase
 {
     public function testUnknownOptionIsNull()
@@ -87,6 +146,17 @@ class HTML_Common2_Test extends PHPUnit2_Framework_TestCase
             array('baz' => 'quux', 'xyzzy' => 'xyzzy value'), 
             $obj->getAttributes()
         );
+    }
+
+    public function testAttributeValuesAreStrings()
+    {
+        $obj = new HTML_Common2_Concrete();
+        $obj->setAttributes(array('foo' => null, 'bar' => 10));
+        $obj->setAttribute('baz', 2.5);
+        $obj->mergeAttributes(array('foobar' => 42));
+        foreach ($obj->getAttributes() as $attribute) {
+            $this->assertType('string', $attribute);
+        }
     }
 
     public function testDefaultIndentLevelIsZero()
