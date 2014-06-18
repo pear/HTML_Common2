61ec5db052b5e6a68e7ff6ead3448991902a28c6
Alexey Borzov <avb@php.net>
2014-06-18 16:58:02 +0400

    Updated copyright year, got rid of SVN keywords

--- HTML/Common2.php
+++ HTML/Common2.php
@@ -6,7 +6,7 @@
  *
  * LICENSE:
  *
- * Copyright (c) 2004-2012, Alexey Borzov <avb@php.net>
+ * Copyright (c) 2004-2014, Alexey Borzov <avb@php.net>
  *
  * All rights reserved.
  *
@@ -38,7 +38,6 @@
  * @package  HTML_Common2
  * @author   Alexey Borzov <avb@php.net>
  * @license  http://opensource.org/licenses/bsd-license.php New BSD License
- * @version  SVN: $Id$
  * @link     http://pear.php.net/package/HTML_Common2
  */
 
